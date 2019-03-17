04287ae9c1614a05f0118d0d064cdc1d19f50a39
j4sysiak <j4sysiak@gmail.com>
2019-03-17 19:08:24 +0100

    1

---
+++ spring-tutorial-Spring-Improving-the-Application-126/src/com/jaceksysiak/spring/web/dao/Offer.java
@@ -0,0 +1,114 @@
+package com.jaceksysiak.spring.web.dao;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.jaceksysiak.spring.web.validation.ValidEmail;
+import com.sun.istack.internal.NotNull;
+
+public class Offer {
+	
+	private int id;
+	 
+	@Size(min=2, max=100)
+	private String name;
+	
+	//@NotNull
+	//@ValidEmail(min=2)
+	private String email;
+	
+	@Size(min=2, max=255)
+	private String text;
+	
+	public Offer() {}
+
+	public Offer(String name, String email, String text) {
+		this.name = name;
+		this.email = email;
+		this.text = text;
+	}
+	
+	public Offer(int id, String name, String email, String text) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.text = text;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "Offer [id=" + id + ", name=" + name + ", email=" + email
+				+ ", text=" + text + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Offer other = (Offer) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+
+}
